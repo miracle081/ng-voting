c2a88c3541e6bfb4c229bc09773572fd0cedd71b
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-11-13 16:03:14 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import { Intro } from './Framework/Screens/Intro';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Intro />
+    </View>
+  );
+}
---
+++ Framework/Screens/Intro.js
@@ -0,0 +1,30 @@
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { AppButton } from '../Components/AppButton';
+
+export function Intro() {
+    return (
+        <ImageBackground source={require("../../assets/intro.jpg")} style={{ width: "100%", height: "100%" }}>
+            <View style={styles.container}>
+                <View></View>
+                <Text style={styles.header}>NG Voting</Text>
+                <AppButton>Get Started</AppButton>
+            </View>
+        </ImageBackground >
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "space-between",
+        padding: 20,
+        backgroundColor: "#00180082"
+    },
+    header: {
+        fontSize: 30,
+        textAlign: "center",
+        fontWeight: "700",
+        color: "white"
+    }
+});
